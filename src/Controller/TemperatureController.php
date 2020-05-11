2c6745e931cbecb0bc32c5644acff7b7c89c8792
robst <robertst81@gmail.com>
2020-05-11 21:09:58 +0200

    add TemperatureController with route for temperature by city

---
+++ src/Controller/TemperatureController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use App\Core\Service\TemperatureService;
+use App\OpenWeatherMap\Exception\CityNotFoundException;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class TemperatureController
+ * @Route("/temperature")
+ */
+class TemperatureController extends AbstractController
+{
+    /**
+     * @Route("/by_city", name="temperature", methods={"POST"})
+     */
+    public function by_city(Request $request, TemperatureService $service)
+    {
+        try {
+
+            return $this->json($service->getTemperatureForCity($request->request->get('city')));
+        } catch (CityNotFoundException $e) {
+
+            return $this->json(["message" => "city not found"], 404);;
+        }
+    }
+}
